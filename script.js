78a3b230d711a1565781cd322ac87816f35a8f1a
Beast <95392136+KBailey-95@users.noreply.github.com>
2022-01-19 23:02:01 -0500

    changed whiles to ifs to fix infinite error. Utiliized math.random and strings to create random password.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function writePassword() {
   console.log(passLength);
 
   //check for appropriate password length
-  while (passLength < 8 || passLength > 128) {
+  if (passLength < 8 || passLength > 128) {
     passLength = prompt("The length of your password must be between 8 and 128 chracters. How long would you like your password to be?");
   }
 
@@ -18,24 +18,22 @@ function writePassword() {
   var isSpecChar = window.confirm("Would you like special characters in your password?");
 
   //check to make sure at least one of the character types is used
-  while (!(isLowerCase || isUpperCase || isNumeric || isSpecChar)){
+  if (!(isLowerCase || isUpperCase || isNumeric || isSpecChar)){
 
     window.alert("Please select at least one character type so that a password can be generated.")
     writePassword();
-    
-    generatePassword();
-
   }
+  generatePassword(isLowerCase, isUpperCase, isNumeric, isSpecChar, passLength);
 }
 
   // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 //create function to generate password
-function generatePassword(){
-  var createdPassword = "pls";
+function generatePassword(isLowerCase, isUpperCase, isNumeric, isSpecChar, passLength){
+  var createdPassword = "";
   console.log(createdPassword);
 
- //create array to store allowed characters based on value of booleans created with window.confirm
+ //create string to store allowed characters based on value of booleans created with window.confirm
  var allowed = "";
   if (isLowerCase) {
     allowed += "qwertyuiopasdfghjklzxcvbnm";
@@ -49,69 +47,16 @@ function generatePassword(){
   if (isSpecChar) {
     allowed += "!@#$%^&*(){}[]=<>/,.";
   }
- 
-  console.log(allowed);
 
+  //loop through to create a value for each character in passLength
+  for (let i = 0; i < passLength; i++) {
+    createdPassword += allowed[Math.floor(Math.random() * allowed.length)];
+  }
+ 
+  //select textarea and return created password to it
   var passwordText = document.querySelector("#password");
 
   passwordText.value = createdPassword;
 
 
 };
-
-  
-//   //create logic for function that takes confirm results and generates password
-//   const passRandomizer = {
-//     lower: getRandomLower,
-//     upper: getRandomUpper,
-//     number: getRandomNumber,
-//     symbol: getRandomSymbol
-//   };
-
-//   function generatePassword(lower, upper, number, symbol, length) {
-//     let generatedPassword = '';
-//     const typesCount = lower + upper + number + symbol;
-//     const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-  
-//     // create a loop
-//     for (var i = 0; i < length; i += typesCount) {
-//       typesArr.forEach(type => {
-//         const funcName = Object.keys(type)[0];
-//         generatedPassword += randomFunc[funcName]();
-//       });
-//     }
-
-//     var displayedPassword = generatedPassword.slice(0, length);
-
-//     return displayedPassword;
-//   }
-
-//   // Generator functions
-// function getRandomLower() {
-//   return rando("qwertyuiopasdfghjklzxcvbnm")
-// }
-
-// function getRandomUpper() {
-//   return rando("QWERTYUIOPASDFGHJKLZXCVBNM");
-// }
-
-// function getRandomNumber() {
-//   return rando(9);
-// }
-
-// function getRandomSymbol() {
-//   return rando('!@#$%^&*(){}[]=<>/,.');
-// }
-
-
-//   // //Determine what happens when each condition is true
-//   // if (isLowerCase = true) {
-//   //   password.toLowerCase()
-//   // }
-//   // if (isUpperCase.toUpperCase)
-
-//   //create variable that communicates where password will be stored
-  
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
